2e6692a3ceedc4ee0209479297e95bf28c26fe12
Felipe Gangrel <felipefrog@gmail.com>
2023-01-14 14:58:23 -0300

    Updates Header component with LinkButton

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden'
+import { FiCheckSquare } from 'react-icons/fi'
 import { DarkModeToggler } from '@/components/DarkModeToggler'
+import { LinkButton } from '@/components/LinkButton'
 import * as S from './Header.styles'
 
 const Header: React.FC = () => {
   return (
     <S.Header>
-      <div>logo</div>
+      <div>
+        <LinkButton href='/'>
+          <FiCheckSquare />
+          <VisuallyHidden.Root>Home</VisuallyHidden.Root>
+        </LinkButton>
+      </div>
       <div>menu</div>
       <div>
         <DarkModeToggler />
