a9313ccb466b1ca0e9d84410767f8e130d65e921
Felipe Gangrel <felipefrog@gmail.com>
2023-01-14 14:05:47 -0300

    Creates DarkModeToggler component

---
+++ src/components/DarkModeToggler/DarkModeToggler.styles.ts
@@ -0,0 +1,4 @@
+import { styled } from "@root/stitches.config";
+import { Button } from "@/components/Button";
+
+export const ToggleButton = styled(Button, {});
---
+++ src/components/DarkModeToggler/DarkModeToggler.tsx
@@ -0,0 +1,34 @@
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { FiMoon, FiSun } from "react-icons/fi";
+import * as S from "./DarkModeToggler.styles";
+
+const DarkModeToggler: React.FC = () => {
+  const { theme, systemTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  const isDark =
+    theme === "dark" || (theme === "system" && systemTheme === "dark");
+
+  const toggleTheme = () => {
+    setTheme(isDark ? "light" : "dark");
+  };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <S.ToggleButton onClick={toggleTheme}>
+      {isDark ? <FiSun /> : <FiMoon />}
+      <VisuallyHidden.Root>Toggle Dark and Light mode</VisuallyHidden.Root>
+    </S.ToggleButton>
+  );
+};
+
+export default DarkModeToggler;
