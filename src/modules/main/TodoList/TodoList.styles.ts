c761d88c09273c84505bee9a918db7425d14ed4e
Felipe Gangrel <felipefrog@gmail.com>
2023-01-15 14:36:55 -0300

    WIP: Updates todo list

--- src/modules/main/TodoList/TodoList.styles.ts
+++ src/modules/main/TodoList/TodoList.styles.ts
@@ -1,3 +1,4 @@
+import { Button } from '@/components/Button/Button.styles'
 import { styled } from '@root/stitches.config'
 
 export const TodoList = styled('div', {
@@ -10,11 +11,41 @@ export const TodoItem = styled('div', {
   display: 'flex',
   gap: '$2',
   alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: '$2',
+  padding: '$4',
   borderRadius: '$2',
   backgroundColor: '$slate2',
+  boxShadow: '$1',
+
   '&:hover': {
     backgroundColor: '$slate3',
+    boxShadow: '$2',
   },
 })
+
+export const ToggleButton = styled(Button, {
+  padding: '$1 $1',
+  color: '$slate7',
+
+  '&:hover': {
+    color: '$slate11',
+    background: 'transparent',
+  },
+
+  variants: {
+    checked: {
+      true: {
+        color: '$indigo11',
+      },
+    },
+  },
+})
+
+const Column = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$2',
+})
+
+export const StatusColumn = styled(Column)
+
+export const MainColumn = styled(Column)
--- src/modules/main/TodoList/TodoList.tsx
+++ src/modules/main/TodoList/TodoList.tsx
@@ -1,3 +1,4 @@
+import { FiCheckCircle } from 'react-icons/fi'
 import type { Todo } from '@/contracts/todo'
 import * as S from './TodoList.styles'
 
@@ -9,7 +10,16 @@ const TodoList: React.FC<TodoListProps> = ({ todos }) => {
   return (
     <S.TodoList>
       {todos.map((todo) => (
-        <S.TodoItem key={todo.id}>{todo.title}</S.TodoItem>
+        <S.TodoItem key={todo.id}>
+          <S.StatusColumn>
+            <S.ToggleButton>
+              <FiCheckCircle size={20} />
+            </S.ToggleButton>
+          </S.StatusColumn>
+          <S.MainColumn>
+            <h4>{todo.title}</h4>
+          </S.MainColumn>
+        </S.TodoItem>
       ))}
     </S.TodoList>
   )
